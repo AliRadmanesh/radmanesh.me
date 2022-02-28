62bad6149c0f82e51b5e156d49051dff493f21e6
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-02-28 13:12:48 +0330

    Add tailwindcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Arial: ["Arial", "sans-serif"],
+    },
+    colors: {
+      "navy-blue": "#073B4C",
+      "light-white": "#F8F9FA",
+      gray: "#847E89",
+      green: "#06D6A0",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
