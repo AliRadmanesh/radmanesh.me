d35b1adfd4fdc42fb96ccc42af44aa421ca1fae0
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-02-28 13:14:27 +0330

    Add CSS reset & Arial font to project root

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import Header from "../components/Header";
+// import Header from "../components/Header";
+
+import "../public/fonts/Arial/stylesheet.css";
+import "../styles/reset.min.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <main className="container">
+      <main>
         <Component {...pageProps} />
       </main>
     </>
