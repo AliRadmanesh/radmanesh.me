9973d1b43ebb33f34f8ac3a4702682221e8f493d
ali.radmanesh <ali.radmanesh1995@gmail.com>
2024-11-22 13:41:36 +0330

    feat: add OpenSans variable font

---
+++ apps/web/utils/fonts.ts
@@ -0,0 +1,25 @@
+import localFont from 'next/font/local';
+
+export const openSansVariableFont = localFont({
+  src: '../public/fonts/OpenSans-VariableFont_wdth,wght.ttf', // localFont looks for the physical file paths relative to where it's imported, so can't use absolute imports!
+  variable: '--font-open-sans',
+  display: 'swap',
+});
+
+/**
+ * This is for static fonts, it will be used when the user's browser doesn't support variable font.
+ */
+// export const openSansStaticFonts = localFont({
+//   src: [
+//     {
+//       path: '../public/fonts/OpenSans-Regular.woff2',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/OpenSans-Bold.woff2',
+//       weight: '700',
+//       style: 'bold',
+//     },
+//   ],
+// });
