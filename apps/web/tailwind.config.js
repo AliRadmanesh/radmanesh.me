70279c985549bd8ce2ec812bcc0186a1d619edc0
ali.radmanesh <ali.radmanesh1995@gmail.com>
2024-11-22 14:33:37 +0330

    chore: define sans to OpenSans font

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+        sans: ['"Open Sans"', 'sans-serif'],
+			},
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
