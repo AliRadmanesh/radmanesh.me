960770d9fef8ae1e4ca16f128c82cd71cbd3dcf2
ali.radmanesh <ali.radmanesh1995@gmail.com>
2024-12-25 01:05:25 +0330

    feat: complete header component

--- apps/web/components/common/header/header.tsx
+++ apps/web/components/common/header/header.tsx
@@ -1,30 +1,59 @@
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/utils/css";
-import { CodeXml } from "lucide-react";
+import { CodeXml, Menu } from "lucide-react";
 import Link from "next/link";
 import { headerItems } from "./data";
 
 import styles from "./styles.module.css";
 
-export const Header = () => {
-	return (
-		<div className="container h-header mx-auto px-4 md:px-6 lg:px-8">
-			<header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
-				<Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
-					<CodeXml className="h-8 w-8" />
-				</Link>
-				<div className="ml-auto flex gap-2">
-					{headerItems.map((item) => (
-						<Link
-							key={item.name}
-							href={item.href}
-							className={cn("group", styles["header-item"])}
-							prefetch={false}
+const HeaderItems = ({ mobile }: { mobile?: boolean }) =>
+	headerItems.map((item) => (
+		<Link
+			key={item.name}
+			href={item.href}
+			className={cn(
+				styles["header-item"],
+				mobile && styles["header-item-mobile"],
+			)}
+			prefetch={false}
+		>
+			{item.name}
+		</Link>
+	));
+
+export const Header = () => (
+	<header className="sticky top-0 z-50 w-full outline outline-1 outline-border bg-white dark:outline-gray-800 dark:bg-gray-950">
+		<div className="container mx-auto flex h-header max-w-6xl items-center justify-between px-4 md:px-6">
+			<Link href="/" className="flex items-center gap-2" prefetch={false}>
+				<CodeXml className="h-8 w-8" />
+				<span className="sr-only">Website icon</span>
+			</Link>
+			<nav className="hidden items-center gap-10 text-sm font-medium md:flex">
+				<HeaderItems />
+			</nav>
+			<div className="flex md:hidden items-center gap-4">
+				<Sheet>
+					<SheetTrigger asChild>
+						<Button
+							variant="ghost"
+							size="icon"
+							className="rounded-full md:hidden"
 						>
-							{item.name}
-						</Link>
-					))}
-				</div>
-			</header>
+							<Menu className="h-8 w-8 text-gray-500 dark:text-gray-400" />
+							<span className="sr-only">Toggle navigation menu</span>
+						</Button>
+					</SheetTrigger>
+					<SheetContent
+						side="top"
+						className="md:hidden fixed top-0 left-0 w-screen h-screen bg-white dark:bg-gray-950"
+					>
+						<div className="grid gap-4 p-4 mt-4 h-3/4 text-center justify-center items-center">
+							<HeaderItems mobile />
+						</div>
+					</SheetContent>
+				</Sheet>
+			</div>
 		</div>
-	);
-};
+	</header>
+);
