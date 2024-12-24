cea27dddc47ed936fd500a79ae8652d8258706a8
ali.radmanesh <ali.radmanesh1995@gmail.com>
2024-12-24 22:48:07 +0330

    chore: make text center

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
 	return (
-		<main className="h-screen flex flex-col items-center justify-center">
-			<h1 className="text-3xl font-bold text-secondary-gray">Ah shit, <span className="text-3xl font-bold text-primary">here we go again.</span></h1>
+		<main className="h-[calc(100vh-var(--tw-h-header))] flex flex-col items-center justify-center">
+			<h1 className="text-3xl font-bold text-secondary-gray">
+				Ah shit,{" "}
+				<span className="text-3xl font-bold text-primary">
+					here we go again.
+				</span>
+			</h1>
 		</main>
 	);
 }
