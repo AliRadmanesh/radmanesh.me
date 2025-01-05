6051035366a6dc1251653997c55990baeb87b1ea
ali.radmanesh <ali.radmanesh1995@gmail.com>
2025-01-05 22:55:35 +0330

    feat(Images): hide scrollbar & change border-radius on mobile

--- apps/web/pages/home/components/images.tsx
+++ apps/web/pages/home/components/images.tsx
@@ -1,11 +1,18 @@
 import { Container } from "@/components/common/container";
+import { cn } from "@/utils/css";
 import Image from "next/image";
+import styles from "./styles.module.css";
 
 const Images = () => {
 	return (
-		<Container className="lg:flex lg:justify-center mt-20 w-full max-w-[100vw] overflow-x-auto overflow-y-hidden no-scrollbar">
+		<Container
+			className={cn(
+				styles["no-scrollbar"],
+				"lg:flex lg:justify-center mt-20 w-full max-w-[100vw] overflow-x-auto overflow-y-hidden",
+			)}
+		>
 			<div className="flex gap-2 w-max">
-				<div className="w-[166.5px] lg:w-[333px] h-[327px] lg:h-[654px] overflow-hidden rounded-xl">
+				<div className="w-[166.5px] lg:w-[333px] h-[327px] lg:h-[654px] overflow-hidden rounded-e-xl lg:rounded-xl">
 					<Image
 						src="/images/mobile.webp"
 						alt="Mobile Design"
@@ -14,7 +21,7 @@ const Images = () => {
 						height={654}
 					/>
 				</div>
-				<div className="w-[512px] lg:w-[1024px] h-[327px] lg:h-[654px] overflow-hidden rounded-xl">
+				<div className="w-[512px] lg:w-[1024px] h-[327px] lg:h-[654px] overflow-hidden rounded-s-xl lg:rounded-xl">
 					<Image
 						src="/images/desktop.webp"
 						alt="Desktop Design"
